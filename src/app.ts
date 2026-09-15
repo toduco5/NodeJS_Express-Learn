2574cfd6b8c084931a4a39669176777b21870c12
toduco5 <tminhduc1304@gmail.com>
2026-09-16 02:07:45 +0700

    config ejs

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 // const express = require("express");
 import express from "express"
+import 'dotenv/config'
+
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT ||8080;
+
+// config views engine
+app.set('view engine', 'ejs')
+app.set('views', 'src/views')
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("homePage");
 });
 app.get("/user", (req, res) => {
   res.send("Hello To Duc");
@@ -11,4 +18,5 @@ app.get("/user", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Running at http://localhost:${PORT}`);
+  console.log(process.env.PORT)
 });
